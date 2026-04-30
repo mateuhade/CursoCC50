06027169cf2bd9b17d8f82a49e8ebb59cca83637
mateuhade <tartarohades552@gmail.com>
2026-04-30 19:30:20 -0300

    refactor: remove useless code from grayscale function

--- 4Memory/exercises/filter-less/helpers.c
+++ 4Memory/exercises/filter-less/helpers.c
@@ -1,14 +1,7 @@
 #include "helpers.h"
-#include <stdio.h>
 
-typedef int8_t COLOR;
-const int red = 1;
-const int green = 2;
-const int blue = 3;
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
-    int lowestColorValue;
-    COLOR *buffer;
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (image[i][j].rgbtBlue < image[i][j].rgbtGreen && image[i][j].rgbtBlue < image[i][j].rgbtRed) {
