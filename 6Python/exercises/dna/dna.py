240951627f9b5466fa3b0ad0df0532ef23cb18ae
mateuhade <mateusmonteiro0408@gmail.com>
2026-06-07 02:40:32 -0300

    feat: add python exercises (part 3)

--- 6Python/exercises/dna/dna.py
+++ 6Python/exercises/dna/dna.py
@@ -4,21 +4,38 @@ import sys
 
 def main():
 
+    # Check for incorrect usage of command
     if len(sys.argv) != 3:
-        print("Usage: dna.py [database] [dnaSequence]")
+        print("Usage: python dna.py [databases/size.csv] [sequences/0.txt]")
         return 1
 
-    database = open(sys.argv[1], "r")
-    reader = csv.DictReader(database)
-    keys = reader.fieldnames
+    # Creates a list containing dictionaries of every name and dna subsequence of every person in the database
+    listOfDatabaseDicts = []
+    with open(sys.argv[1]) as file:
+        reader = csv.DictReader(file)
+        for item in reader:
+            listOfDatabaseDicts.append(item)
 
-    dnaSequence = open(sys.argv[2], "r")
-    print({database['name']})
+    # Stores the dna sequence that the subsequences will be compared to in a string
+    dnaSequence = ""
+    with open(sys.argv[2]) as file:
+        for STR in file:
+            dnaSequence += STR
 
-        
-    # TODO: Find longest match of each STR in DNA sequence
+    # Creates a list containing each header from the database, item 0 being the name of the person and the subsequent ones
+    # being the dna subsequences
+    dnaSubsequences = []
+    for key in listOfDatabaseDicts[0]:
+        dnaSubsequences.append(key)
 
-    # TODO: Check database for matching profiles
+    # Creates a list containing the number of matches for each subsequence in the dna sequence
+    dnaMatches = []
+    for subsequence in range(1, len(dnaSubsequences)):
+        currentMatch = longest_match(dnaSequence, dnaSubsequences[subsequence])
+        dnaMatches.append(currentMatch)
+
+    dnaSearchResult = check_for_dna_match(listOfDatabaseDicts, dnaSubsequences, dnaMatches)
+    print(dnaSearchResult)
 
     return
 
@@ -59,6 +76,24 @@ def longest_match(sequence, subsequence):
 
     # After checking for runs at each character in seqeuence, return longest run found
     return longest_run
+
+def check_for_dna_match(database, dnaSubsequences, dnaMatches):
+
+    # TODO: Check database for matching profiles
+    isCurrentPersonMatch = True
+    for person in database:
+        
+        for subsequence in range(1, len(dnaSubsequences)):
+            currentSubsequence = dnaSubsequences[subsequence]
+            if not (dnaMatches[subsequence-1] == int(person[currentSubsequence])):
+                isCurrentPersonMatch = False
+
+        if isCurrentPersonMatch:
+            return person["name"]
+        
+        isCurrentPersonMatch = True
+    
+    return "No match found"
 
 
 main()
