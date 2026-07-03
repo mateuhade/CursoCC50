ef63d51dbd1ecf5f9a531eb8b5fb0941e56d3895
mateuhade <mateusmonteiro0408@gmail.com>
2026-07-03 19:40:31 -0300

    feat: add Web exercises (part 4)

--- 8Web/exercises/Homepage/static/js/coffee.js
+++ 8Web/exercises/Homepage/static/js/coffee.js
@@ -1,4 +1,4 @@
-const coffeeFile = document.querySelector("#coffeeFile");
+const coffeeFile = document.querySelector("body");
 const coffeeSubmit = coffeeFile.querySelector("#coffeeSubmit");
 const coffeeInput = coffeeFile.querySelector("#coffeeInput");
 const coffeeStatus = coffeeFile.querySelector("#audioStatus")
@@ -7,36 +7,35 @@ coffeeSubmit.addEventListener("click", function() {
     const coffeeImage = coffeeFile.querySelector("#resultImage");
     const coffeeAudio = coffeeFile.querySelector("#resultAudio");
     let input = (coffeeInput.value).replace(/[^a-zA-Z]/g, "").toUpperCase()
+    revealVibe(coffeeImage, coffeeAudio, input)
+})
 
+// pops up image and song based on the coffee Input sent by the user
+function revealVibe(coffeeImage, coffeeAudio, input) {
+    let isValid = true;
     let image = "", audio = "";
     if (input.includes("AMERICANO")){
-        audio = "/static/audios/Cafezinho-CyroMonteiro.mp3";
-        coffeeAudio.setAttribute("src", audio);
         coffeeStatus.textContent = "Now Playing: Cafezinho - Cyro Monteiro"
-
+        audio = "/static/audios/Cafezinho-CyroMonteiro.mp3";
         image = "/static/images/americano.jpg"
-        coffeeImage.setAttribute("src", image);
     }
     else if (input.includes("MOCHA")){
-        audio = "/static/audios/NonBreathOblige-PinocchioP.mp3";
-        coffeeAudio.setAttribute("src", audio);
         coffeeStatus.textContent = "Now Playing: Non-Breath Oblige - PinocchioP"
-
+        audio = "/static/audios/NonBreathOblige-PinocchioP.mp3";
         image = "/static/images/mocha.jpg"
-        coffeeImage.setAttribute("src", image);
     }
     else if (input.includes("ESPRESSO")){
-        audio = "/static/audios/IWantToBeCremated-ABuSe.mp3";
-        coffeeAudio.setAttribute("src", audio);
         coffeeStatus.textContent = "Now Playing: I Want To Be Cremated - ABuSe"
-
+        audio = "/static/audios/IWantToBeCremated-ABuSe.mp3";
         image = "/static/images/espresso.jpg"
-        coffeeImage.setAttribute("src", image);
     }
     else {
         alert("No, it's not.");
+        isValid = false;
     }
-})
 
-// pops up image based on the coffee Input Inputd by the user
-function popImage(coffeeInput) {}
+    if (isValid) {
+        coffeeAudio.setAttribute("src", audio);
+        coffeeImage.setAttribute("src", image);
+    }
+}
